7c5b584aa74c91666e3c204c697c9b4111b74a7a
KennethAlAr <kennethalonsoarce@gmail.com>
2026-02-20 22:55:46 +0100

    Router de libros creado

---
+++ src/routes/books.routes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const {
+    getAllBooks,
+    getBookByID,
+    getBookByAuthor,
+    getBookByPublisher,
+    getBookByYear,
+    getBookByISBN,
+    createBook,
+    deleteBook,
+    updateBook,
+} = require("../controllers/books.controllers");
+
+const router = express.Router();
+
+router.get("/", getAllBooks);
+router.get("/:id", getBookByID);
+router.get("/author/:author", getBookByAuthor);
+router.get("/publisher/:publisher", getBookByPublisher);
+router.get("/year/:year", getBookByYear);
+router.get("/isbn/:isbn", getBookByISBN);
+router.post("/", createBook);
+router.delete("/:id", deleteBook);
+router.put("/:id", updateBook);
+
+module.exports = router
