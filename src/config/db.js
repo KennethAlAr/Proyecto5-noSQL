0924549d7223c08b7416c41346e180ab386d3810
KennethAlAr <kennethalonsoarce@gmail.com>
2026-02-20 22:16:44 +0100

    first commit

---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const MONGO_URI = "mongodb://localhost:27017/proyecto5-noSQL"
+
+const connect = async () => {
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log("✅ Conexión con la base de datos correcta.");
+    } catch (error) {
+        console.log("⛔ Error al conectar con la base de datos.")
+    };
+}
+
+module.exports = {connect};
